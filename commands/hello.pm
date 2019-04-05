9150024f11142d0149da4aae8be9e306faf610c7
Михаил <webyska@yandex.ru>
2019-04-05 19:32:01 +0300

    Update hello.pm

--- commands/hello.pm
+++ commands/hello.pm
@@ -104,6 +104,17 @@ sub fem {
 
 }
 
+my @varlamov = (
+    -178831229
+);
+sub varlamov {
+    my $peer_id = $_[0]->{'object'}->{'peer_id'};
+    my $post = requests::sender::wall_get(@varlamov);
+    requests::sender::message_send($peer_id,'', $post);
+    #warn(Dumper($jconf->{'response'}->{'items'}->[0]->{'id'}))
+
+}
+
 sub boxes {
     my $peer_id = $_[0]->{'object'}->{'peer_id'};
     my @photo = requests::sender::photo_get_random("478080307","259469122");
@@ -212,6 +223,11 @@ sub hime {
     requests::sender::message_send($peer_id,'', 'video475072121_456239025');
 }
 
+sub bio {
+    my $peer_id = $_[0]->{'object'}->{'peer_id'};
+    requests::sender::message_send($peer_id,'', 'video-55093819_456240747');
+}
+
 commands::commandHandler::createCommand("химе", \&hime);
 commands::commandHandler::createCommand("меси", \&elizabeth);
 commands::commandHandler::createCommand("кровосток", \&krovostok);
@@ -230,4 +246,6 @@ commands::commandHandler::createCommand("фем", \&fem);
 commands::commandHandler::createCommand("хуемрази", \&fem);
 commands::commandHandler::createCommand("хуемразь", \&fem);
 commands::commandHandler::createCommand("хуе-мрази", \&fem);
+commands::commandHandler::createCommand("биография", \&bio);
+commands::commandHandler::createCommand("многоэтажка", \&varlamov);
 1;
